667dedc10a26e5b219e2c9a112bbcbfda7486fb0
reinfajar <tsanimfajar@outlook.com>
2020-03-19 11:01:45 +0700

    Changes to be committed: 	modified:   app.js

--- app.js
+++ app.js
@@ -3,8 +3,11 @@ require('dotenv').config()
 const express = require('express')
 const app = express()
 const http = require('http').createServer(app)
+const io = require('socket.io')(http)
 const PORT = process.env.PORT
 
-
+io.on('connection', ()=> {
+    console.log('User Connected')
+})
 
 http.listen(PORT, ()=> console.log(`listening on ${PORT}`))
