8aebd3f59ce4c58146db444852439313164963ad
reinfajar <tsanimfajar@outlook.com>
2020-03-19 22:41:10 +0700

    done basic  Changes to be committed: 	modified:   app.js

--- app.js
+++ app.js
@@ -6,10 +6,29 @@ const http = require('http').createServer(app)
 const io = require('socket.io')(http)
 const PORT = process.env.PORT
 
-io.on('connection', ()=> {
+io.on('connection', (socket)=> {
+    // ! User Connect
     console.log('a User Connected')
+    // ? score
+    socket.on('send-score', data => {
+      console.log('send-score', data)
+      // ? broadcast-score
+      socket.broadcast.emit('on-broadcast-score', data)
+    })
+    socket.on('join-game', player => {
+      console.log('join-game', player)
+      // ? player join
+      socket.broadcast.emit('on-player-joined', player)
+    })
+
+    socket.on('on-start-game', _=> {
+      console.log('game-start', )
+      socket.broadcast.emit('on-start-game', )
+    })
+
+    // ! User Disconnect
     socket.on('disconnect', ()=> {
-        console.log('a User Disconnected')
+      console.log('a User Disconnected')
     })
 })
 
