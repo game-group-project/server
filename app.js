68f87c8815c93288ef63f70ba973b3239c98b31d
reinfajar <tsanimfajar@outlook.com>
2020-03-20 10:52:09 +0700

    edit

--- app.js
+++ app.js
@@ -5,37 +5,36 @@ const app = express()
 const http = require('http').createServer(app)
 const io = require('socket.io')(http)
 const PORT = process.env.PORT
-const players = []
+let players = []
 
 io.on('connection', (socket)=> {
     // ! User Connect
     console.log('a User Connected')
     // ? score
     socket.on('send-score', data => {
-      console.log('send-score', data)
       // ? broadcast-score
       socket.broadcast.emit('on-broadcast-score', data)
     })
     socket.on('join-game', player => {
-      // console.log('join-game', player)
       players.push(player)
       // ? player join
       io.emit('on-player-joined', players)
     })
 
     socket.on('start-game', _=> {
-      console.log('game-start', )
       socket.broadcast.emit('on-start-game')
     })
 
     socket.on('restart-game', _=> {
-      console.log('restart')
-      socket.broadcast.emit('on-start-game')
+      players = []
+      socket.broadcast.emit('on-restart-game')
     })
 
     // ! User Disconnect
     socket.on('disconnect', ()=> {
       console.log('a User Disconnected')
+      players = []
+      socket.broadcast.emit('on-restart-game')
     })
 })
 
